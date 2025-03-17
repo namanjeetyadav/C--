a13eca6ce1e94eecf5c23aae103b27a29a099908
namanjeet <namanshuttler@gmail.com>
2025-03-18 02:31:16 +0530

    18.03.25

---
+++ basics.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int a =10;
+void sum(){
+    cout<<a<<"\n";
+}
+
+int main(){
+    int a=7;
+    a=80;
+    sum();
+    cout<<"This is name \n"<<a;
+
+    return 0;
+}
