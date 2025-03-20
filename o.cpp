caac383275a4a8fed1cb4a1c88bebbc4c8446381
namanjeet <namanshuttler@gmail.com>
2025-03-20 23:39:18 +0530

    20.03.25

--- o.cpp
+++ o.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int a;
+int c = 45; //This is a global variable
+float s = 5.5f; //use f to tell that this is a float value 
+// otherwise after passing this value it is treated as double
+
+long double h = 5.223l;
+
+int main()
+{
+    int a, b, c;
+    cout<<"Enter the value of a "<<endl;
     cin>>a;
-    cout<<a;
+    cout<<"Enter the value of b "<<endl;
+    cin>>b;
+    c=a+b;
+    cout<<"Sum is "<<c;
+
+    return 0;
 }
