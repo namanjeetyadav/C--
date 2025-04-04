e3fcf9686dc7ca2859ceb71b5d5a6635c5295a11
namanjeet <namanshuttler@gmail.com>
2025-04-04 19:21:17 +0530

    04.04.25

---
+++ nested_mem.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class binary{
+    private: 
+    string s;
+    public:
+    void read(void);
+    void ones_compliment(void);
+    void display(void);
+
+
+};
+
+void binary :: read()
+{
+    cout<<"Enter a number "<<endl;
+    cin>>s;
+}
+void binary :: ones_compliment()
+{
+    
+}
+
+void binary :: display()
+{
+    
+}
+
+
+
+
+int main(){
+
+    return 0;
+}
