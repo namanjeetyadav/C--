89564937da2c14ef56f0a9238a6a98121ee1ad6a
namanjeet <namanshuttler@gmail.com>
2025-05-08 14:52:44 +0530

    08.05.25

--- oops1.cpp
+++ oops1.cpp
@@ -4,34 +4,32 @@ using namespace std;
 class Employee
 {
 private:
-int a, b, c;
+    int a, b, c;
 public:
-int d, e;
+int d,e;
 void setData(int a1, int b1, int c1);
 void getData(){
-cout<<"The vale of a is "<<a<<endl;
-cout<<"The vale of b is "<<b<<endl;
-cout<<"The vale of c is "<<c<<endl;
-cout<<"The vale of d is "<<d<<endl;
-cout<<"The vale of e is "<<e<<endl;
+ cout<<"The value of a is "<<a<<endl;
+ cout<<"The value of b is "<<b<<endl;
+ cout<<"The value of c is "<<c<<endl;
+ cout<<"The value of d is "<<d<<endl;
+ cout<<"The value of e is "<<e<<endl;
 
 }
-
 };
 
-
-void Employee :: setData(int a1, int b1, int c1){
-    a = a1;
-    b = b1;
-    c = c1;
+void Employee :: setData(int a1, int b1,int c1){
+a = a1;
+b = b1;
+c = c1;
 }
 
 int main(){
     Employee naman;
-    naman.d = 55;
-    naman.e = 11;
+    naman.d =12;
+    naman.e = 15;
     naman.setData(1,2,3);
     naman.getData();
-
     return 0;
 }
+
