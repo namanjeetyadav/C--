097f8d6f3836e0fc5e6227607809934b2b6b4f2c
namanjeet <namanshuttler@gmail.com>
2025-05-14 01:32:47 +0530

    14.05.25

--- static.cpp
+++ static.cpp
@@ -1,40 +1,41 @@
+//  a static variable is a special type of variable that is shared among all objects of a class, rather than having a separate copy for each object.
+
 #include <iostream>
 using namespace std;
 
 class Employee
 {
     int id;
-    static int count;
-  
+    int static count;
+
 
     public:
-    void setData(void)
-    {
-        cout<<"Enter the id"<<endl;
+     void setData(void){
+        cout<<"Enter the id ";
         cin>>id;
         count++;
-    }
-    void getData(void){
-        cout<<"The id of this employee is "<<id<<" and this is employee number "<<count<<endl;
-    }
-     static void getCount(void){
-        //cout<<id; //Throws error 
-        cout<<"The value of count is "<<count<<endl;
+     }
+     void getData(void){
+        cout<<"The id of employee is "<<id<<" and this is the employee number "<<count<<endl;
+     }
+    static void getCount(void){
+        cout<<"The value of the count is "<<count<<endl;
+
     }
 };
-int Employee:: count;
+
+int Employee :: count;
 
 int main(){
-    Employee naman, aman , shivam;
-    //naman.id=1;
-    //naman.count=1; //cannot do this beacuse id and count are private
-naman.setData();
-naman.getData();
-aman.setData();
-aman.getData();
-shivam.setData();
-shivam.getData();
-// Employee::getCount();
-
-return 0;
+    Employee naman, aman, shivam;
+    //naman.id =1;  //cannot do bcz id is private
+    // naman.count = 1; //cannot do bcz count is private
+    naman.setData();
+    naman.getData();
+    aman.setData();
+    aman.getData();
+    shivam.setData();
+    shivam.getData();
+    Employee :: getCount();
+    return 0;
 }
