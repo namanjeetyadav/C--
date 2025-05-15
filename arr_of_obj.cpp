800635d00dea5207adb355edde13a9d8a861f1c7
namanjeet <namanshuttler@gmail.com>
2025-05-15 13:58:24 +0530

    15.05.25

--- arr_of_obj.cpp
+++ arr_of_obj.cpp
@@ -13,19 +13,23 @@ public:
         cout << "Enter the id of employee" << endl;
         cin >> id;
     }
+
     void getId()
     {
-        cout << "The id of this employee is " << id << endl;
+        cout << "The id of the employee is " << id << endl;
     }
 };
 
 int main()
 {
     Employee arr_of_obj[4];
+
     for (int i = 0; i < 4; i++)
-    { 
+    {
+
         arr_of_obj[i].setId();
         arr_of_obj[i].getId();
     }
+
     return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,31 @@
-b.display();
+#include <iostream>
+using namespace std;
+
+class Employee
+{
+    int id;
+    int salary;
+
+public:
+    void setId(void)
+    {
+        salary = 122;
+        cout << "Enter the id of employee" << endl;
+        cin >> id;
+    }
+    void getId()
+    {
+        cout << "The id of this employee is " << id << endl;
+    }
+};
+
+int main()
+{
+    Employee arr_of_obj[4];
+    for (int i = 0; i < 4; i++)
+    {
+        arr_of_obj[i].setId();
+        arr_of_obj[i].getId();
+    }
+    return 0;
+}
